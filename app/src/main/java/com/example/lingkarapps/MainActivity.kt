d49f5013a4ceac71d73a0530087b0a16e7869f0b
Farhan <farhan03com@gmail.com>
2024-01-12 20:29:45 +0800

    kedua

--- app/src/main/java/com/example/lingkarapps/MainActivity.kt
+++ app/src/main/java/com/example/lingkarapps/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.lingkarapps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick: Button = findViewById(R.id.buttonClick)
+
     }
+
+
 }
